8387e8d21f7a2cca2f527e34b0fd592fe8360b77
michaelvlach <vlach.michael@gmail.com>
2021-02-10 21:20:51 +0100

    Add tokenizer

---
+++ projects/adbquery/compiler/tokenizer.js
@@ -0,0 +1,133 @@
+// Copyright 2020 Agnesoft
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+let pos = 0;
+let data = "";
+
+function get() {
+    return data.charAt(pos++);
+}
+
+function identifierType(value) {
+    return ["if", "else", "for"].includes(value) ? "keyword" : "identifier";
+}
+
+function isAlphaNumeric(c) {
+    return /[a-zA-Z\d]/.test(c);
+}
+
+function isCharacter(c) {
+    return /[a-zA-Z]/.test(c);
+}
+
+function isInteger(c) {
+    return /[\d]/.test(c);
+}
+
+function isOperator(c) {
+    return /[+=<>]/.test(c);
+}
+
+function isPunctuation(c) {
+    return /[().,]/.test(c);
+}
+
+function isWhitespace(c) {
+    return /\s/.test(c);
+}
+
+function hasNext() {
+    return pos < data.length;
+}
+
+function peek() {
+    return data.charAt(pos);
+}
+
+function readIdentifier() {
+    const value = readWord(isAlphaNumeric);
+
+    return {
+        type: identifierType(value),
+        value: value,
+    };
+}
+
+function readNumber() {
+    const number = readWord(isAlphaNumeric);
+    validateNumber(number);
+
+    return {
+        type: "number",
+        value: Number(number),
+    };
+}
+
+function readOperator() {
+    return {
+        type: "operator",
+        value: readWord(isOperator),
+    };
+}
+
+function readPunctuation() {
+    return {
+        type: "punctuation",
+        value: get(),
+    };
+}
+
+function readWord(test) {
+    let word = "";
+
+    do {
+        word += get();
+    } while (hasNext() && test(peek()));
+
+    return word;
+}
+
+function validateNumber(num) {
+    if (isNaN(num)) {
+        throw `'${num}' is not a number.`;
+    }
+}
+
+export function setData(expressionData) {
+    pos = 0;
+    data = expressionData;
+}
+
+export function next() {
+    while (hasNext()) {
+        const c = peek();
+
+        if (isWhitespace(c)) {
+            pos++;
+            continue;
+        } else if (isInteger(c)) {
+            return readNumber();
+        } else if (isCharacter(c)) {
+            return readIdentifier();
+        } else if (isPunctuation(c)) {
+            return readPunctuation();
+        } else if (isOperator(c)) {
+            return readOperator();
+        } else {
+            throw `Cannot handle character '${c}'.`;
+        }
+    }
+
+    return undefined;
+}
---
+++ projects/adbquery/test/tokenizer.test.js
@@ -0,0 +1,184 @@
+// Copyright 2020 Agnesoft
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import * as tokenizer from "../compiler/tokenizer.js";
+
+describe("serializer", () => {
+    describe("valid", () => {
+        test("number", () => {
+            tokenizer.setData("   123   ");
+
+            expect(tokenizer.next()).toEqual({
+                type: "number",
+                value: 123,
+            });
+        });
+
+        test("keyword", () => {
+            tokenizer.setData("if (");
+
+            expect(tokenizer.next()).toEqual({
+                type: "keyword",
+                value: "if",
+            });
+        });
+
+        test("identifier", () => {
+            tokenizer.setData("\nId");
+
+            expect(tokenizer.next()).toEqual({
+                type: "identifier",
+                value: "Id",
+            });
+        });
+
+        test("operator", () => {
+            tokenizer.setData("+=1");
+
+            expect(tokenizer.next()).toEqual({
+                type: "operator",
+                value: "+=",
+            });
+        });
+
+        test("punctuation", () => {
+            tokenizer.setData("(val");
+
+            expect(tokenizer.next()).toEqual({
+                type: "punctuation",
+                value: "(",
+            });
+        });
+
+        test("function name", () => {
+            tokenizer.setData("foo()");
+
+            expect(tokenizer.next()).toEqual({
+                type: "identifier",
+                value: "foo",
+            });
+        });
+
+        test("function with arguments", () => {
+            tokenizer.setData("foo(arg1, arg2)");
+            let tokens = [];
+            let token = undefined;
+
+            while ((token = tokenizer.next())) {
+                tokens.push(token);
+            }
+
+            const expected = [
+                {
+                    type: "identifier",
+                    value: "foo",
+                },
+                {
+                    type: "punctuation",
+                    value: "(",
+                },
+                {
+                    type: "identifier",
+                    value: "arg1",
+                },
+                {
+                    type: "punctuation",
+                    value: ",",
+                },
+                {
+                    type: "identifier",
+                    value: "arg2",
+                },
+                {
+                    type: "punctuation",
+                    value: ")",
+                },
+            ];
+
+            expect(tokens).toEqual(expected);
+        });
+
+        test("function with nested call", () => {
+            tokenizer.setData("foo(arg1, bar())");
+            let tokens = [];
+            let token = undefined;
+
+            while ((token = tokenizer.next())) {
+                tokens.push(token);
+            }
+
+            const expected = [
+                {
+                    type: "identifier",
+                    value: "foo",
+                },
+                {
+                    type: "punctuation",
+                    value: "(",
+                },
+                {
+                    type: "identifier",
+                    value: "arg1",
+                },
+                {
+                    type: "punctuation",
+                    value: ",",
+                },
+                {
+                    type: "identifier",
+                    value: "bar",
+                },
+                {
+                    type: "punctuation",
+                    value: "(",
+                },
+                {
+                    type: "punctuation",
+                    value: ")",
+                },
+                {
+                    type: "punctuation",
+                    value: ")",
+                },
+            ];
+
+            expect(tokens).toEqual(expected);
+        });
+    });
+
+    describe("invalid", () => {
+        test("bad token at the start", () => {
+            tokenizer.setData("~");
+
+            expect(tokenizer.next).toThrow("Cannot handle character '~'.");
+        });
+
+        test("bad token in the middle", () => {
+            tokenizer.setData("foo~()");
+
+            const next = () => {
+                tokenizer.next();
+                tokenizer.next();
+            };
+
+            expect(next).toThrow("Cannot handle character '~'.");
+        });
+
+        test("bad number", () => {
+            tokenizer.setData("123a");
+
+            expect(tokenizer.next).toThrow("'123a' is not a number.");
+        });
+    });
+});

85e3cd33476ebebe1e76c17f3058e009ab96a452
michaelvlach <vlach.michael@gmail.com>
2021-02-20 21:31:42 +0100

    Update compiler.test.js

--- projects/adbquery/test/compiler.test.js
+++ projects/adbquery/test/compiler.test.js
@@ -17,35 +17,49 @@ import * as compiler from "../compiler/compiler.js";
 describe("serializer", () => {
     describe("valid", () => {
         test("sample scheme", () => {
-            const ast = compiler.compile(
-                JSON.stringify({
-                    FizzBuzz: "string",
-                    Id: "int64",
-                    MyArr: ["Id"],
-                    MyVariant: ["Id", "MyArr"],
-                    print: {
-                        arguments: ["string"],
-                        body: [],
-                    },
-                    MyObj: {
-                        fields: ["Id"],
-                        functions: {
-                            doFizzBuzz: {
-                                arguments: ["int64"],
-                                body: ["if (int64 == 15) { print(FizzBuzz) }"],
-                            },
-                            fizzBuzz: {
-                                body: ["for(Id) { doFizzBuzz(i) }"],
-                            },
+            const data = JSON.stringify({
+                FizzBuzz: "string",
+                Id: "int64",
+                MyArr: ["Id"],
+                MyVariant: ["Id", "MyArr"],
+                print: {
+                    arguments: ["string"],
+                    body: [],
+                },
+                MyObj: {
+                    fields: ["Id"],
+                    functions: {
+                        doFizzBuzz: {
+                            arguments: ["int64"],
+                            body: ["if (int64 == 15) { print(FizzBuzz) }"],
+                        },
+                        fizzBuzz: {
+                            body: ["for(Id) { doFizzBuzz(i) }"],
                         },
                     },
-                    main: {
-                        body: ["MyObj(20)", "MyObj.fizzBuzz()"],
-                    },
-                })
-            );
+                },
+                main: {
+                    body: ["MyObj(20)", "MyObj.fizzBuzz()"],
+                },
+            });
 
-            expect(ast).toEqual({});
+            const compile = () => {
+                return compiler.compile(data);
+            };
+
+            expect(compile).not.toThrow();
+        });
+    });
+
+    describe("invalid", () => {
+        test("invalid json", () => {
+            const data = "{";
+
+            const compile = () => {
+                return compiler.compile(data);
+            };
+
+            expect(compile).toThrow("Unexpected end of JSON input");
         });
     });
 });
